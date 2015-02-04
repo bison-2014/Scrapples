bb4a33426068b8929430908424dac5547c08aaee
Lele Duan <duanstar@gmail.com>
2015-02-04 17:38:49 -0600

    added player_id uniqueness validation for round model

--- app/models/round.rb
+++ app/models/round.rb
@@ -1,7 +1,11 @@
 class Round < ActiveRecord::Base
 
-    belongs_to :game
+  belongs_to :game
 
-    has_many :appearances, through: :game
-    has_many :players, through: :appearances
+  has_many :appearances, through: :game
+  has_many :players, through: :appearances
+
+  validates :player_id, uniqueness: { scope: :game_id,
+    message: "must be unique for each game" }
+
 end
