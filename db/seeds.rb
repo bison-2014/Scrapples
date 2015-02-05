b34f0a52c49b0af2e2155e0bcbfcc6c8a413f352
Raj Singh <raj@foreversingh.com>
2015-02-05 12:37:43 -0600

    fixed seeding hopefully

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
 
 CSV.foreach('db/cardset.csv', headers: true) do |line|
   Card.create(name: line['Name'], bio: line['Text'], category: line['Category'])
