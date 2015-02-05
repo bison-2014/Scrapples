70e1f87eaa5e7ae78c310b706c18be77fd9a6c3c
Dicko Sow <s12.dsow@gmail.com>
2015-02-04 22:49:42 -0600

    added user sign up page, rspec, and force_ssl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,50 @@
 class UsersController < ApplicationController
 
+  def index
+  end
+
   def new
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
-    @user = User.new(params[:user])
+    @user = User.new(user_params)
     # @user.password = params[:password]
 
     if @user.save
-      flash[:notice] = "You have signed up successfully"
+      flash[:success] = "You have signed up successfully"
       # flash[:color] = "Valid"
       session[:user_id] = @user.id
+      redirect_to @user
     else
-      flash[:notice] = "Form is invalid"
+      # flash[:notice] = "Form is invalid"
       # flash[:color] = "Invalid"
+    render :new
     end
-    render "new"
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
   has_secure_password
+  before_save { self.email = email.downcase }
+
+  # EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+  EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+
+  validates :password, confirmation: true 
+  validates_length_of :password, :in => 6..20, :on => :create
 
 end
