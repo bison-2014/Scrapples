236c5a4278c516feec817d25aef622e6c66d68a1
Lele Duan <duanstar@gmail.com>
2015-02-05 12:05:14 -0600

    added intial code for games controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,7 +5,11 @@ class GamesController < ApplicationController
     end
 
     def create
+        game = Game.create!
+        appearance = game.appearances.create!(player: current_user)
+        round = game.rounds.create!
 
+        redirect game_round_path
     end
 
 end
