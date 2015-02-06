cd2dcfb034641109c39191075840f5b444065d51
Lele Duan <duanstar@gmail.com>
2015-02-06 11:19:34 -0600

    fixed redirect path in Game#create.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,25 @@
 class GamesController < ApplicationController
 
-    def new
-        render :new
-    end
 
-    def create
-        game = Game.create!
-        appearance = game.appearances.create!(player: current_user)
-        round = game.rounds.create!
+  def new
+    render :new
+  end
 
-        redirect game_round_path
-    end
+  def create
+    game = Game.create!
+    appearance = game.appearances.create!(player: current_user)
+
+    redirect_to game_path(game)
+  end
+
+  private
+
+  # def first_round
+  #   self.rounds.first
+  # end
+
+  def last_round
+    self.rounds.last
+  end
 
 end
