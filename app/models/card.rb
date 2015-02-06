ffa1c738e90659085be0239401c3b372bbd367b8
Raj Singh <raj@foreversingh.com>
2015-02-06 11:21:31 -0600

    passing spec tests

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,7 +2,8 @@ class Card < ActiveRecord::Base
   has_many :holdings
   has_many :plays
   # has_many :played_rounds, through: :plays
-  validates_presence_of :name, :bio
+  
+  validates :name, :bio, presence: true, on: :create
   validates_uniqueness_of :name
   
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -1,38 +1,34 @@
 require 'spec_helper'
 require 'rails_helper'
 
-Describe "Card" do 
+describe "Card" do 
   let(:name) { "Jim Bob" }
   let(:bio) { 'One huge text blob that will go through a whole history of the person on the card' }
-  let(:card) { Card.new(name, bio)}
+  let(:card) { Card.create(name: name, bio: bio) }
   
-  context '#initialize' do
+  context '#create' do
     it 'creates a Card object' do
       expect(card).to be_a Card
     end
     
     it 'requires two parameters' do
-      expect { Card.new }.to raise_error(ArgumentError)
-    end
-    
-    it "initializes with a created_at timestamp" do 
-      expect(task.created_at).to be_a DateTime
+      expect { Card.create! }.to raise_error
     end
   end
       
-  describe '#title' do 
-    it 'has a title attribute' do 
-      expect(card).to respond_to(:title)
+  describe '#name' do 
+    it 'has a name attribute' do 
+      expect(card).to respond_to(:name)
     end
     
-    it 'has a title as a string' do 
-      expect(card.title).to be_a String
+    it 'has a name as a string' do 
+      expect(card.name).to be_a String
     end
   end
   
   describe '#bio' do
     it 'has a bio attribute' do
-      expect.card.to respond_to(:bio)
+      expect(card).to respond_to(:bio)
     end
     
     it 'has a bio as a large text block' do 
