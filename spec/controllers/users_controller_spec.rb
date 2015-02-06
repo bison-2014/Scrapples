d385f676fafc6060b1952867f79a51eafd225282
Dicko Sow <s12.dsow@gmail.com>
2015-02-06 09:58:42 -0600

    created model and controller specs for user

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,45 +1,60 @@
+require 'spec_helper'
 require 'rails_helper'
 
 describe UsersController do
+  let!(:miriam) {User.create!(name: "Miriam", email: "mm@me.com", password: "password", password_confirmation: "password")}
 
   describe 'GET #new' do
+    before { get :new }
+
     it 'renders successfully' do
-      get :new
       expect(response).to be_success
     end
 
-    it 'renders the new page' do
-      get :new
+    it 'renders the new template' do
       expect(response).to render_template(:new)
     end
   end
 
   describe 'POST #create' do
-      let(:user) do
+    let(:nicole) do
       {name: "nicole", email: "nicole@me.com", password: "password", password_confirmation: "password"}
     end
 
-    it 'allows user to sign up' do
-      post :create, user: user
+    before { post :create, user: nicole }
 
+    it 'allows user to sign up' do
       expect(response).to redirect_to user_path(User.last)
-      expect(controller.current_user).to eq User.last
+      expect(controller.current_user).to eq(User.last)
     end
   end
 
   describe 'GET #edit' do
+    before { get :edit, id: miriam.id }
+  
     it 'renders successfully' do
-      user = users(:dicko)
-      get edit_user_path(user)
-       assert_template 'users/edit'
-    patch user_path(@user), user: { name:  "Dicko",
-                                    email: "foo@invalid.com",
-                                    password: "foo",
-                                    password_confirmation: "foo" }
-    assert_template 'users/edit'
+      expect(response).to be_success
     end
 
+    it 'renders edit template' do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'PATCH#update' do
+    before { put :update, user: {email: "miriam@me.com" }, id: miriam.id}
+
+    it "will direct user to show page" do
+      expect(response).to redirect_to miriam
+    end
+  end
+
+  describe 'GET#show' do
+    before { get :show, id: miriam.id }
+
+    it 'renders show template' do
+      expect(response).to render_template(:show)
+    end
   end
 
 end
- 
