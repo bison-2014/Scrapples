95c4c00f6c3ffd14ee9eaa3077b84ffc35b1d354
s12dsow <s12.dsow@gmail.com>
2015-02-05 19:09:34 -0600

    user controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,38 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# describe UsersController do
+describe UsersController do
 
-# end
+  describe 'GET #new' do
+    it 'renders successfully' do
+      get :new
+      expect(response).to be_success
+    end
+
+    it 'renders the new page' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'POST #create' do
+      let(:user) do
+      {name: "nicole", email: "nicole@me.com", password: "password", password_confirmation: "password"}
+    end
+
+    it 'allows user to sign up' do
+      post :create, user: user
+
+      expect(response).to redirect_to user_path(User.last)
+      expect(controller.current_user).to eq User.last
+    end
+  end
+
+  describe 'GET #edit' do
+    it 'renders successfully' do
+      get :edit
+      expect(response).to be_success
+    end
+
+  end
+
+end
