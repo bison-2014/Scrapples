900d7c99af78c7f83ef4ec9958cc0761c70c6739
Lele Duan <duanstar@gmail.com>
2015-02-07 23:17:11 -0600

    fixed plays spec

--- spec/models/plays_spec.rb
+++ spec/models/plays_spec.rb
@@ -1,1 +1,13 @@
-plays_spec.rb
+include 'rails-helper'
+
+# describe Play do
+#   describe "#create" do
+#     it "creates a new play" do
+
+#     end
+
+#     it "only creates one play per appearence/round combination" do
+#     end
+
+#   end
+# end
