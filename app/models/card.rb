54844cfb1eb49db8ca51fa6d23cd320a45598d8a
Zamyatin <raj@foreversingh.com>
2015-02-07 23:33:53 -0600

    removed card relation to a round because nobody cares

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,9 +1,8 @@
 class Card < ActiveRecord::Base
   has_many :holdings
   has_many :plays
-  # has_many :played_rounds, through: :plays
+
   
   validates :name, :bio, presence: true, on: :create
-  validates_uniqueness_of :name
   
 end
