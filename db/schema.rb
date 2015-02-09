5cd882e0dca643b6dd51dfdda24990354196a6e2
Zamyatin <raj@foreversingh.com>
2015-02-08 20:36:56 -0600

    added placeholder images to cards

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150208053030) do
+ActiveRecord::Schema.define(version: 20150209003753) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20150208053030) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "img_url"
   end
 
   create_table "cast_votes", force: true do |t|
